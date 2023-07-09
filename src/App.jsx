f399d13370eb1e0f1631cb67f28990103293fad0
Askari <askarir64@gmail.com>
2023-07-10 03:45:26 +0530

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,56 @@
 import "./App.css";
+import Card from "./components/Card";
 import { MdSearch } from "react-icons/md";
+import { useState } from "react";
+import axios from 'axios'
+
+const apiKey = import.meta.env.API_KEY;
+
+
+function CardHandler() {
+    
+  return (
+    <Card temperature="38" 
+    units="C" name="Lucknow" country="India"
+      icon="//cdn.weatherapi.com/weather/64x64/night/389.png"
+      text="Moderate or heavy rain with thunder"
+      windspeed='30 kph' humidity='78' feelslike='30'
+      visibility='2' visibilityUnit='KM' UV='1'
+    />
+  )
+}
 
 function App() {
+  const [city, setCity] = useState('');
+
+  const handleClick = () => {
+    console.log(city);
+  };
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+
   return (
     <>
       <div>
         <div className="bg">
           <h1 className="heading">Weatherion</h1>
           <div className="form-container">
-            <input className="form" type="form" placeholder="Location" />
-            <button className="search-button">
+            <input
+              className="form"
+              type="text"
+              placeholder="Location"
+              value={city}
+              onChange={handleChange}
+            />
+            <button onClick={handleClick} className="search-button">
               <MdSearch />
             </button>
           </div>
         </div>
-        <div className="Card-container">
-          <div className="Card">
-            <h1>Hello</h1>
-            <p>Lorem Ipsum</p>
-          </div>
+        <div className="Card-container-primary">
+          <CardHandler />
         </div>
       </div>
     </>
---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Card(props) {
+
+
+  return (
+    <div className='card-component'>
+    <div className='weather-condition'>
+    <h2 className='location'>{props.name}, {props.country}</h2>
+        <p>{props.temperature} - {props.units}</p>
+        <img src={props.icon}/>
+        </div>
+        <p>{props.text}</p>
+        <p>Wind Speed - {props.windspeed}</p>
+        <p>Humidity - {props.humidity}</p>
+        <p>Feels like - {props.feelslike}</p>
+        <p>Visibility - {props.visibility} {props.visibilityUnit}</p>
+        <p>UV - {props.UV}</p>
+    </div>
+  )
+}
+
+export default Card
+ 
+//is_day toggle day-night theme
